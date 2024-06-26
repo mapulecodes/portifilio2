730bc3dcd40635ebd4f8c5d2d0eedfbd6c9eed47
mapulecodes <mbuzwanamapule@gmail.com>
2024-06-26 16:31:24 +0200

    landing page

---
+++ src/components/LandingSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="row">
+        <div class="row">
+            <div class="col">
+                <img src="https://mapulecodes.github.io/fridayimages/images/1.png" alt="profile" class="img-fluid" loading="lazy">
+            </div>
+            <div class="col">
+                <div id="details">
+                    <h1 class="display-1">Mapule Mbuzwana</h1>
+                    <span>{{ jobTitle[0].title }}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+    import { computed, onMounted } from 'vue';
+    import { useStore } from 'vuex'
+    const store = usestore()
+    const jobTitle =
+        computed(() => this.$store.state.jobTitle)
+onMounted(() => {
+    this.$store.dispatch('fetchJobTitle')
+})
+     
+
+
+</script>
+
+<style scoped>
+</style>
