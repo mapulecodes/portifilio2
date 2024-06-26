a6db77abb118efc874df0d15767985d8cea95642
mapulecodes <mbuzwanamapule@gmail.com>
2024-06-26 11:02:49 +0200

    navbar-Setup

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+ <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <router-link class="navbar-brand" href="#">
+        <img src="https://mapulecodes.github.io/fridayimages/images/OIP.jpg" alt="logo" class="img-fluid" loading="lazy">
+    </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#resume">Resume</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#project">project</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#testimonial">testimonial</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#reachMe">reachMe</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+<style>
+
+</style>
