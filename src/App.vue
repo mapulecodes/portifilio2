933f4d704d8e31918cdda19802873a540e32c2c2
mapulecodes <mbuzwanamapule@gmail.com>
2024-06-26 11:40:04 +0200

    Footer setup

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,16 @@
     <main class="container-fluid">
       <router-view />
     </main>
+    <Footer />
   </div>
-   
-  
-  <router-view/>
 </template>
 <script>
 import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
 export default {
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 
 }
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="container-fluid">
+    <div class="row">
+        <p class="lead">
+            Mapule's portfolio &copy;
+            <span class="shadow">{{ cYear }}</span>
+        </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+name: "FooterComp",
+data() {
+    return {
+        cYear: new Date().cYeargetUTCFullYear()
+    }
+},
+}
+</script>
+
+<style>
+
+</style>
