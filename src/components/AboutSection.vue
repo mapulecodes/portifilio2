054d1143fed3df1ba2e6c48f442f4333f212ebf2
mapulecodes <mbuzwanamapule@gmail.com>
2024-06-27 12:49:12 +0200

    AboutSection

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+        <img src="https://mapulecodes.github.io/fridayimages/images/mecover.png" alt="" class="img-fluid" loading="lazy">
+    </div>
+    <div class="row">
+        <div v-if="about?.length">
+         <p class="lead" v-for="(self, index ) in about" :key="index">
+             {{ self }}
+         </p>
+        </div>
+        <Spinner v-else/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import Spinner from './Spinner.vue'
+    import { computed, onMounted, ref } from 'vue';
+    import { useStore } from 'vuex'
+    const store = useStore()
+    const about=
+        computed(() => store.state.jobTitle)
+      
+onMounted(() => {
+    store.dispatch('fetchJobTitle')
+    repeat()
+})
+</script>
+
+<style>
+
+</style>
